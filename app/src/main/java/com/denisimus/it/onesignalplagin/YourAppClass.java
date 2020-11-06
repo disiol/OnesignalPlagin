a3a4c3241b31ef00de944af3584fedf7ff25aec7
denisum <deoniisii@gamail.com>
2020-11-06 23:00:35 +0200

    notification.payload.additionalData.keys()

--- app/src/main/java/com/denisimus/it/onesignalplagin/YourAppClass.java
+++ app/src/main/java/com/denisimus/it/onesignalplagin/YourAppClass.java
@@ -1,14 +1,37 @@
 package com.denisimus.it.onesignalplagin;
+
+import com.onesignal.OSNotification;
+import com.onesignal.OneSignal;
 import com.unity3d.player.UnityPlayer;
 
-public class YourAppClass {
+import org.json.JSONException;
+
+import java.util.Iterator;
+
+public class YourAppClass implements OneSignal.NotificationReceivedHandler  {
 
     public static void start(String id) {
-        OneSignal.init(UnityPlayer.currentActivity, (String)null, id);
+        OneSignal.init(UnityPlayer.currentActivity, (String) null, id);
     }
 
-    public static void nobot() {
-        OneSignal.sendTag("nobot", "1");
+    public static void sendTag(String key, String value) {
+        OneSignal.sendTag(key, value);
+    }
+
+    @Override
+    public void notificationReceived(OSNotification notification) {
+
+        Iterator<String> keys = notification.payload.additionalData.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+
+            try {
+                OneSignal.sendTag(key, notification.payload.additionalData.get(key).toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
 
